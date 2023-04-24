e763a2b0d819bc73a6b3265a0075d61ac36f87b6
Hwang jiwon <sweet.waiting0l@icloud.com>
2023-04-24 21:38:52 +0900

    feat: Print buzz
    Condition: i is multiple of 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,13 +6,13 @@ def do_fizzbuzz(num:int):
     15: fizzbuzz
     etc: num
     """
-    for i in range(1, num + 1):
-        if i % 3 == 0:
+    for i in range(1,num+1):
+        if i%3==0:
             print('fizz')
+        elif i%5==0:
+            print('buzz')
         else:
             print(f'{i}')
-
-    print('hello')
 
 
 if __name__=='__main__':
